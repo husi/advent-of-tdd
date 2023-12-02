86e78864c71ebb2e0ebcf87bbebb8e50f1b0aef6
Krisztian Notaisz <krisztian.notaisz@gmail.com>
2023-12-02 11:34:09 +0100

    full solution

--- rust/day_1/src/main.rs
+++ rust/day_1/src/main.rs
@@ -1,68 +1,161 @@
 mod extractor {
+    use fancy_regex::CaptureMatches;
     pub trait Extractor {
-        fn find_first_num<I>(&self, chars: I) -> Option<u32>
-            where I: Iterator<Item=char>;
+        fn parse_line(&self, line: &str) -> (u32, u32);
     }
 
-    pub fn extract_first_num(line: &str, extractor: &impl Extractor) -> Result<u32, String> {
-        extractor.find_first_num(line.chars()).ok_or("No number".to_string())
+    pub fn get_bounding_matches(matches: &mut CaptureMatches) -> (String, String) {
+        let first = String::from(&(matches.next().unwrap().unwrap()[1]));
+        let second = if let Some(value) = matches.last() {
+            String::from(&(value.unwrap()[1]))
+        } else {
+            first.clone()
+        };
+        (first, second)
     }
 
-    pub fn extract_last_num(line: &str, extractor: &impl Extractor) -> Result<u32, String> {
-        extractor.find_first_num(line.chars().rev()).ok_or("No number".to_string())
+    pub fn compute(input: Vec<String>, extractor: &impl Extractor) -> u32 {
+        input.iter()
+            .map(|l| {
+                let (first, last) = extractor.parse_line(l);
+                10 * first + last
+            }).sum()
     }
 
-    pub struct SimpleExtractor {}
+
+}
+
+mod exercise1 {
+    use fancy_regex::Regex;
+
+    use common::load_aoc_input;
+
+    use crate::extractor::{Extractor, get_bounding_matches};
+    use crate::extractor::compute as common_compute;
+
+    struct SimpleExtractor {
+        re: Regex,
+    }
+
+    impl SimpleExtractor {
+        fn new() -> Self {
+            Self { re: Regex::new(r"(?=(\d))").unwrap() }
+        }
+
+    }
 
     impl Extractor for SimpleExtractor {
-        fn find_first_num<I>(&self, mut chars: I) -> Option<u32> where I: Iterator<Item=char> {
-            chars.find(|c| { c >= &'0' && c <= &'9' }).map(|c| { c.to_digit(10).unwrap() })
+        fn parse_line(&self, line: &str) -> (u32, u32) {
+            let mut matches = self.re.captures_iter(line);
+            let (first, second) = get_bounding_matches(&mut matches);
+            (first.parse().unwrap(), second.parse().unwrap())
+        }
+    }
+
+    pub fn compute(input_file: &str) -> u32 {
+        let  input = load_aoc_input(input_file);
+        let extractor = SimpleExtractor::new();
+        common_compute(input, &extractor)
+    }
+
+
+    #[cfg(test)]
+    mod tests{
+        use super::*;
+
+        #[test]
+        fn test_extractor() {
+            let extractor = SimpleExtractor::new();
+
+            assert_eq!(extractor.parse_line("1234"), (1,4));
+            assert_eq!(extractor.parse_line("1"), (1,1));
+            assert_eq!(extractor.parse_line("sadsa1das23da4dasda"), (1,4));
+            assert_eq!(extractor.parse_line("asdas1dasda"), (1,1))
+        }
+        #[test]
+        fn test_with_example() {
+            assert_eq!(compute("test_data/e1.txt"), 142)
         }
     }
 
 }
 
-mod exercise1 {
+mod exercise2 {
+    use fancy_regex::Regex;
+
     use common::load_aoc_input;
-    use crate::extractor::{extract_first_num, extract_last_num, SimpleExtractor};
+
+    use crate::extractor::{Extractor, get_bounding_matches, compute as common_compute};
+
+    const NUMBERS:[&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    struct AdvancedExtractor {
+        re: Regex
+    }
+
+    impl AdvancedExtractor {
+        fn new() -> Self {
+            let pattern = String::from(r"(?=(") + &NUMBERS.join("|") + r"|\d))";
+            Self{re: Regex::new(&pattern).unwrap()}
+        }
+
+        fn parse_number(num: &str) -> u32 {
+            match NUMBERS.iter().position(|word| *word == num) {
+                Some(position) => (position +1) as u32,
+                None => num.parse().unwrap()
+            }
+        }
+
+    }
+
+    impl Extractor for AdvancedExtractor {
+        fn parse_line(&self, line: &str) -> (u32, u32) {
+            let mut matches = self.re.captures_iter(line);
+            let (first, second) = get_bounding_matches(&mut matches);
+            (Self::parse_number(&first), Self::parse_number(&second))
+        }
+
+    }
+
 
     pub fn compute(input_file: &str) -> u32 {
-        let input = load_aoc_input(input_file);
-        let extractor = SimpleExtractor{};
-        input.iter().map(|l| { 10 * extract_first_num(l,&extractor).unwrap() + extract_last_num(l,&extractor).unwrap() }).sum()
+        let  input = load_aoc_input(input_file);
+        let extractor = AdvancedExtractor::new();
+        common_compute(input, &extractor)
     }
+
+    #[cfg(test)]
+    mod tests{
+        use super::*;
+
+        #[test]
+        fn test_extractor() {
+            let extractor = AdvancedExtractor::new();
+
+            assert_eq!(extractor.parse_line("1234"), (1,4));
+            assert_eq!(extractor.parse_line("1"), (1,1));
+            assert_eq!(extractor.parse_line("sadsa1das23da4dasda"), (1,4));
+            assert_eq!(extractor.parse_line("asdas1dasda"), (1,1));
+            assert_eq!(extractor.parse_line("one"), (1,1));
+            assert_eq!(extractor.parse_line("onetwo"), (1,2));
+            assert_eq!(extractor.parse_line("some1onemore"), (1,1));
+            assert_eq!(extractor.parse_line("fancyeightwoandmore"), (8,2));
+        }
+        #[test]
+        fn test_with_example() {
+            assert_eq!(compute("test_data/e2.txt"), 281)
+        }
+    }
+
 }
 
 fn main() {
-    println!("Excercise1: {}", exercise1::compute("test_data/puzzle1.txt"))
+    println!("Exercise1: {}", exercise1::compute("test_data/puzzle1.txt"));
+    println!("Exercise2: {}", exercise2::compute("test_data/puzzle1.txt"));
 }
 
 
 #[cfg(test)]
 mod test_extractor {
-    use super::extractor;
-
-    #[test]
-    fn test_extract_first_num() {
-        let extractor =extractor::SimpleExtractor{};
-        assert_eq!(extractor::extract_first_num("1234", &extractor).unwrap(), 1);
-        assert_eq!(extractor::extract_first_num("aaa1assaa", &extractor).unwrap(), 1);
-    }
-
-    #[test]
-    fn test_extract_last_num() {
-        let extractor =extractor::SimpleExtractor{};
-        assert_eq!(extractor::extract_last_num("1234", &extractor).unwrap(), 4);
-        assert_eq!(extractor::extract_last_num("aaa1assaa", &extractor).unwrap(), 1);
-    }
 }
 
-#[cfg(test)]
-mod test_exercise1 {
-    use super::exercise1;
 
-    #[test]
-    fn test_with_example() {
-        assert_eq!(exercise1::compute("test_data/e1.txt"), 142)
-    }
-}
